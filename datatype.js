b9a2880834a1825acb72447f60ebfc84f834c722
moneyforey <moneyforey786@gmail.com>
2026-03-20 22:19:33 +0530

    datatype file changes

--- datatype.js
+++ datatype.js
@@ -1,1 +1,14 @@
+//primitive data types
+let a = 10;
+let b = a;
+b=20;
+console.log(a); //a is remain same 
+console.log(b); //b is cahnged
+
+//non-primitive datatype refrence datatypes
+const user1 = {name:"Manish"};
+const user2 = user1;
+user2.name = "Amit";
+console.log(user2)
+console.log(user1)
 
